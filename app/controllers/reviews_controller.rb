9af58c73f42ea133013d93e6f3ed68ed945c004b
Marcos Lutterbach <mlutterb@gmail.com>
2022-05-18 14:06:40 +0200

    adding font awesome icons

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[create new]
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
@@ -14,8 +13,18 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
-  
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
